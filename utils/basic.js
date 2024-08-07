8d492f37d207cb4c47570f8574cb529c483e3fac
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-08 01:03:59 +0300

    Update basic.js

--- utils/basic.js
+++ utils/basic.js
@@ -6,8 +6,6 @@ import { ObjectId } from 'mongodb';
 const basicUtils = {
   /**
    * Checks if Id is Valid for Mongo
-   * @id {string|number} id to be evaluated
-   * @return {boolean} true if valid, false if not
    */
   isValidId(id) {
     try {
