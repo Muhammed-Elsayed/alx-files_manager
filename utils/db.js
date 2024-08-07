58ab7d8d0643e38abdb8d7b6de7fc47ea69676f5
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-08 01:03:41 +0300

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -12,7 +12,6 @@ class DBClient {
   constructor() {
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
       if (!err) {
-        // console.log('Connected successfully to server');
         this.db = client.db(DB_DATABASE);
         this.usersCollection = this.db.collection('users');
         this.filesCollection = this.db.collection('files');
@@ -25,15 +24,13 @@ class DBClient {
 
   /**
    * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
    */
   isAlive() {
     return Boolean(this.db);
   }
 
   /**
-   * Returns the number of documents in the collection users
-   * @return {number} amount of users
+   * Returns documents
    */
   async nbUsers() {
     const numberOfUsers = this.usersCollection.countDocuments();
@@ -42,7 +39,6 @@ class DBClient {
 
   /**
    * Returns the number of documents in the collection files
-   * @return {number} amount of files
    */
   async nbFiles() {
     const numberOfFiles = this.filesCollection.countDocuments();
